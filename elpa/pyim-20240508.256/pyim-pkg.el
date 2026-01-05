d742944459277bb848a0d0c098f5a070ca59d3b9
xieshuqiang <shuqiangxie@outlook.com>
2026-01-05 23:41:02 +0800

    chore: 优化 .gitignore 并清理自动生成的冗余文件

---
+++ elpa/pyim-20240508.256/pyim-pkg.el
@@ -0,0 +1,17 @@
+(define-package "pyim" "20240508.256" "A Chinese input method support quanpin, shuangpin, wubi, cangjie and rime."
+  '((emacs "27.1")
+    (async "1.6")
+    (xr "1.13"))
+  :commit "f22c20f2e6af55b3a758defabe4c842fb94cde2b" :authors
+  '(("Ye Wenbin" . "wenbinye@163.com")
+    ("Feng Shu" . "tumashu@163.com"))
+  :maintainers
+  '(("Feng Shu" . "tumashu@163.com"))
+  :maintainer
+  '("Feng Shu" . "tumashu@163.com")
+  :keywords
+  '("convenience" "chinese" "pinyin" "input-method")
+  :url "https://github.com/tumashu/pyim")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/rime-20241211.808/rime-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "rime" "20241211.808"
+  "Rime input method."
+  '((emacs    "26.3")
+    (dash     "2.17.0")
+    (cl-lib   "0.6.1")
+    (popup    "0.5.3")
+    (posframe "0.1.0"))
+  :url "https://github.com/DogLooksGood/emacs-rime"
+  :commit "80f09ed36d9f0ca7ce4e1a3ca1020dc4c80ba335"
+  :revdesc "80f09ed36d9f"
+  :keywords '("convenience" "input-method"))
