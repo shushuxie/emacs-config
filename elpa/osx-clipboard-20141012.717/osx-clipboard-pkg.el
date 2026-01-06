bec6a4faf64ccc5285874ac104612a6fb151cf1a
xieshuqiang <shuqiangxie@outlook.com>
2026-01-07 00:51:05 +0800

    fix: 重新应用 .gitignore 并清理缓存文件

--- elpa/osx-clipboard-20141012.717/osx-clipboard-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "osx-clipboard" "20141012.717"
-  "Use the OS X clipboard from terminal Emacs."
-  ()
-  :url "https://github.com/joddie/osx-clipboard-mode"
-  :commit "e46dd31327a3f92f77b013b4c9b1e5fdd0e5c73d"
-  :revdesc "e46dd31327a3"
-  :authors '(("Jon Oddie" . "jonxfieldatgmail.com"))
-  :maintainers '(("Jon Oddie" . "jonxfieldatgmail.com")))
